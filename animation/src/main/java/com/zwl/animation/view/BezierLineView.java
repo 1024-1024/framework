213964ffaee2ad1253f59a63008d1d82803ec975
zhangweilong <zhangwl.freedom@gmail.com>
2016-12-19 16:22:53 +0800

    fe

---
+++ animation/src/main/java/com/zwl/animation/view/BezierLineView.java
@@ -0,0 +1,23 @@
+package com.zwl.animation.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by weilongzhang on 16/12/16.
+ */
+
+public class BezierLineView extends View {
+    public BezierLineView(Context context) {
+        super(context);
+    }
+
+    public BezierLineView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BezierLineView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
