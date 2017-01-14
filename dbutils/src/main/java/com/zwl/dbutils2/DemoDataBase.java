5246a0a74995b2379db0bb602a3690ec3d391f71
zhangweilong <zhangwl.freedom@gmail.com>
2017-01-14 11:11:27 +0800

    fix

---
+++ dbutils/src/main/java/com/zwl/dbutils2/DemoDataBase.java
@@ -0,0 +1,26 @@
+package com.zwl.dbutils2;
+
+import android.content.Context;
+
+import com.zwl.App;
+import com.zwl.dbutils2.table.DataCacheTable;
+
+/**
+ * Created by weilongzhang on 17/1/14.
+ */
+
+public class DemoDataBase extends DataBaseHelper {
+
+    public static final String DATABASE_NAME = "test.db";
+    public static final int DATABASE_VERSION = 1;
+
+
+    public DemoDataBase(Context context, String name, int version) {
+        super(App.appContext, DATABASE_NAME, DATABASE_VERSION);
+    }
+
+    @Override
+    public void initTables(DataBaseHelper db) {
+        addTable(DataCacheTable.class, new DataCacheTable(db));
+    }
+}
