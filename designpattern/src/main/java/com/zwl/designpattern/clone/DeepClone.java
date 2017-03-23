d800001807073b04784f9dee1e373653e4e5e029
zhangweilong <zhangwl.freedom@gmail.com>
2017-03-23 12:03:17 +0800

    deepclone

---
+++ designpattern/src/main/java/com/zwl/designpattern/clone/DeepClone.java
@@ -0,0 +1,25 @@
+package com.zwl.designpattern.clone;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * Created by weilongzhang on 17/3/23.
+ */
+
+public class DeepClone {
+
+    public Object deepClone() throws IOException, ClassNotFoundException {
+        //将对象写到流里
+        ByteArrayOutputStream bo = new ByteArrayOutputStream();
+        ObjectOutputStream oo = new ObjectOutputStream(bo);
+        oo.writeObject(this);
+        //从流里读出来
+        ByteArrayInputStream bi = new ByteArrayInputStream(bo.toByteArray());
+        ObjectInputStream oi = new ObjectInputStream(bi);
+        return (oi.readObject());
+    }
+}
