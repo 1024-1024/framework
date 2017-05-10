61cd01a20c0b88650c12dcedeffab937286c15db
1024-1024 <zhangwl.freedom@gmail.com>
2017-05-10 21:57:36 +0800

    statusview

--- statusview/src/main/java/com/example/statusview/MainActivity.java
+++ statusview/src/main/java/com/example/statusview/MainActivity.java
@@ -15,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        setStatusBarColor(R.color.colorAccent));
-//        setStatusBarTrans();
+//        setStatusBarColor(R.color.blue);
+        setStatusBarTrans();
 //        setFullScreen();
     }
 
